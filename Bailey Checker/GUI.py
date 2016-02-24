3d3fe47d03271e65889f169dc20508fab3a9baff
David Smerkous <smerkousdavid@gmail.com>
2016-02-24 10:30:50 -0600

    Update progress bar

--- Bailey Checker/GUI.py
+++ Bailey Checker/GUI.py
@@ -2,23 +2,26 @@ from threading import Thread
 from gi.repository import Gtk, GObject
 
 
-class GUIFace:
-    def __init__(self):
+class ProgressBar:
+    def __init__(self, width, height):
         GObject.threads_init()
-        self.win = Gtk.Window(default_height=50, default_width=300)
+        self.win = Gtk.Window(default_height=height, default_width=width)
         self.win.connect("delete-event", Gtk.main_quit)
+        self.win.resize(width=width, height=height)
+        self.win.set_resizable(False)
 
         self.progress = Gtk.ProgressBar(show_text=True)
         self.win.add(self.progress)
 
     def set_text(self, toset):
-        toset = "" if toset is None else toset
-        self.progress.set_text(str(toset + ": {}%".format(100 * self.progress.get_fraction())))
+        toset = "" if "%" in str("" if toset is None else toset) else toset
+        self.progress.set_text(str(toset + ": {}%".format(100 * self.progress.get_fraction()))
+                               if toset is not "" else str(100 * self.progress.get_fraction()) + " %")
 
     def set_percent(self, percent):
         self.progress.set_fraction(float(percent) / 100)
         current = "Running...:" if self.progress.get_text() is None else self.progress.get_text()
-        self.set_text(str((current[current.index(":"):])))
+        self.set_text(str((current[current.index(":"):])) if ":" in current else "")
 
     def add_percent(self, toadd):
         self.set_percent((100 * self.progress.get_fraction()) + toadd)
--- Bailey Checker/Main.py
+++ Bailey Checker/Main.py
@@ -1,7 +1,7 @@
-from GUI import GUIFace
+from GUI import ProgressBar
 from time import sleep
 
-gui = GUIFace()
+gui = ProgressBar(50, 100)
 
 
 def on_start_app():
@@ -9,15 +9,14 @@ def on_start_app():
     gui.set_text("")
     while True:
         for a in range(100):
-            gui.add_percent(1)
-            gui.set_text("")
+            gui.set_percent(a)
+            gui.set_text("Who cares?")
             sleep(0.1)
-        for a in range(100, 0):
-            gui.remove_percent(1)
-            gui.set_text("")
+        for a in range(100, 0, -1):
+            gui.set_percent(a)
+            gui.set_text("My caring cares")
             sleep(0.1)
         sleep(0.01)
-
 
 if __name__ == "__main__":
     gui.start(on_start_app)
