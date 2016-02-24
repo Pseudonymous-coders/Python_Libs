427a4f3816200e48a994fc014da9ce5b2b69cae2
David Smerkous <smerkousdavid@gmail.com>
2016-02-24 15:13:49 -0600

    files

--- Bailey Checker/GUI.py
+++ Bailey Checker/GUI.py
@@ -2,6 +2,48 @@ from threading import Thread
 from gi.repository import Gtk, GObject
 
 
+class MessageDialog:
+    def __init__(self, title, text):
+        self.message = Gtk.MessageDialog(title=title)
+        self.message.set_border_width(3)
+        self.label = Gtk.Label(text)
+        self.button = Gtk.Button("Okay")
+
+    def connect(self, function):
+        self.button.connect("clicked", function)
+
+    def show(self):
+        box = self.message.get_content_area()
+        box.add(self.label)
+        box.add(self.button)
+        self.label.show()
+        self.button.show()
+        self.message.run()
+
+    def destroy(self):
+        self.message.destroy()
+
+
+class FileDialog:
+    def __init__(self):
+        self.dialog = Gtk.FileChooserDialog("Please choose a file", None,
+                                            Gtk.FileChooserAction.SELECT_FOLDER, buttons=
+                                            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+                                             Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+        self.path = None
+
+    def get_folder(self):
+        response = self.dialog.run()
+        if response == Gtk.ResponseType.CANCEL:
+            print "User hit cancel"
+            self.dialog.destroy()
+            return [True, self.path]
+        self.path = self.dialog.get_filename()
+        print "User opened folder\nPath:", self.path
+        self.dialog.destroy()
+        return [False, self.path]
+
+
 class ProgressBar:
     def __init__(self, width, height):
         GObject.threads_init()
@@ -9,7 +51,6 @@ class ProgressBar:
         self.win.connect("delete-event", Gtk.main_quit)
         self.win.resize(width=width, height=height)
         self.win.set_resizable(False)
-
         self.progress = Gtk.ProgressBar(show_text=True)
         self.win.add(self.progress)
 
@@ -35,3 +76,6 @@ class ProgressBar:
         thread.start()
         self.win.show_all()
         Gtk.main()
+
+    def destroy(self):
+        self.win.destroy()
--- Bailey Checker/Main.py
+++ Bailey Checker/Main.py
@@ -1,11 +1,27 @@
-from GUI import ProgressBar
+from GUI import ProgressBar, MessageDialog, FileDialog
 from time import sleep
 
 gui = ProgressBar(50, 100)
+dialog = FileDialog()
+#message = MessageDialog("ERROR", "Couldn't load the requested folder")
+
+
+def exit_app(na):
+    gui.destroy()
+    #message.destroy()
+    exit(0)
 
 
 def on_start_app():
-    print "App started"
+    print "Analyzer Started"
+    dialog_response = dialog.get_folder()
+    if True:  # dialog_response[0]:
+        print "okay"
+        #message.connect(exit_app)
+        #message.show()
+        # exit_app()
+    path = dialog_response[1]
+    gui.set_percent(0)
     gui.set_text("")
     while True:
         for a in range(100):
@@ -17,6 +33,7 @@ def on_start_app():
             gui.set_text("My caring cares")
             sleep(0.1)
         sleep(0.01)
+
 
 if __name__ == "__main__":
     gui.start(on_start_app)
